260f12ec68d6c6c2c3b139e33334320ba6667352
pdynowski <paul.dynowski@gmail.com>
2015-11-12 12:16:35 -0600

    Factorial added

---
+++ week-4/factorial/my_solution.rb
@@ -0,0 +1,19 @@
+# Factorial
+
+# I worked on this challenge with: Mo.
+
+
+# Your Solution Below
+def factorial(number)
+  # Your code goes here
+  return 1 if number == 0
+  
+  product = number
+  i = number - 1
+  while(i > 0)
+    product = product * i
+    i -= 1
+  end
+  return product
+    
+end
