e71a63f4bb22d721e24258f64e08f65e6f76caf3
pdynowski <paul.dynowski@gmail.com>
2015-11-17 22:32:41 -0600

    Initial setup

---
+++ week-5/acct_groups.rb
@@ -0,0 +1,8 @@
+#Assignment 5.6 Create Accountability Groups
+
+# Split cohort into accountability groups of 4 or 5 people - or, 3, in a pinch
+
+# Input: array containing a list of names
+
+# Output: accountability groups containing at least 3, and preferably 4 or 5
+# people
