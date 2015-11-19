662655ae022b751e3f13ff40754db71080f7ad67
pdynowski <paul.dynowski@gmail.com>
2015-11-19 13:13:17 -0600

    Add reflection

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -75,3 +75,34 @@ def mode(array)
 end
 
 # 4. Reflection
+
+# Which data structure did you and your pair decide to implement and why?
+
+# We decided to implement a hash because we needed to store a value - in this
+# case, the number of occurrences of each object in the initial array - and
+# associate that value with the object. This makes the hash more appropriate
+# in this case than an array would have been.
+
+# Were you more successful breaking this problem down into implementable
+# pseudocode than the last with a pair?
+
+# Yes; I think the pseudocoding is getting easier for everyone as we go along.
+# Still suffered a bit from being a little nonspecific in the pseudocode at
+# times, though.
+
+# What issues/successes did you run into when translating your pseudocode to
+# code?
+
+# I left a bit of pseudocode a bit too open - "grab all entries with maximum
+# values" (from the hash), so we needed to spend a bit of extra time going
+# over that part when we were actually coding.
+
+# What methods did you use to iterate through the content? Did you find any
+# good ones when you were refactoring? Were they difficult to implement?
+
+# We used #each to iterate over the input array, which seemed necessary and
+# appropriate for the task of building a hash from the data in that array. We
+# also used #select to pull the key/value pairs with the maximum values from
+# the hash. The methods were pretty simple to implement, although it did take
+# us a minute to remember that we needed to return only the keyset from the
+# selected pairs.
