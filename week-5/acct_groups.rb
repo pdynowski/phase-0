c335ebb1270f6842e4cb943c6744c5267999e661
pdynowski <paul.dynowski@gmail.com>
2015-11-18 22:01:13 -0600

    Add final code and reflection

--- week-5/acct_groups.rb
+++ week-5/acct_groups.rb
@@ -16,10 +16,11 @@
 	# Caculate number of extra people after dividing into as many
 	# 4-person groups as possible (list length % 4)
 	# IF number of extra people <= number of groups of 4
-		# THEN change that number of groups of 4 to groups of 5
+		# THEN change that number of groups of 4 to groups of 5 starting with the
+		# group in cell 0 and counting up to cell (extra - 1)
 	# ELSE IF number of extra people < 3
 		# THEN change (3-extra) groups of 4 to groups of 3 to create another group
-		# of 3
+		# of 3 starting at group in cell (2-extra) and counting down to 0
 	# ELSE leave final group as group of 3
 
 # Assign names to groups
@@ -94,7 +95,11 @@
 # accountability groups - with a method for creating groups, methods for
 # reading and writing groups to a file, and a method for updating groups
 
-######## Refactored Code with randomizer implemented. Also, made the method nondestructive. Choosing not to implement saving or modification of groups, as I feel that those would require a Class or Module to be properly implemented.
+######## Refactored Code with randomizer implemented. Also, made the method
+######## nondestructive. Choosing not to implement saving or modification of
+######## groups, as I feel that those should require a Class or Module to be
+######## properly implemented. I can't figure out a good way to refactor the
+######## conditionals, so if the reviewer has any tips, please share...
 
 def assign_groups(list)
 	
@@ -132,10 +137,55 @@ def assign_groups(list)
 end
 
 
-# DRIVER CODE
+######## DRIVER CODE
 name_array = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t"]
 for i in 1..20
 	p assign_groups(name_array.slice(0,i))
 end
-# p assign_groups(name_array)
 p name_array
+
+######## REFLECTIONS
+
+# What was the most interesting and most difficult part of this challenge?
+
+# The most intereting part of this challenge, to me, was figuring out how I
+# was going to break the list of students into relatively equal groups. Most
+# difficult was trying to figure out how to refactor the conditional
+# statements - I managed to eliminate a little, but feel like I could have
+# done better.
+
+# Do you feel you are improving in your ability to write pseudocode and break
+# the problem down?
+
+# I do feel that I'm getting better at breaking the problem down, and more
+# specifically at making sure my pseudocode includes steps broken down as far
+# as possible.
+
+
+# Was your approach for automating this task a good solution? What could have
+# made it even better?
+
+# I think that, as far as it goes, my approach was good. It could be made
+# better by including the optimal group size as a parameter, although that
+# would also change my solution for breaking the sizes into groups pretty
+# significantly. Also, I think methods to store, retrieve, and edit the groups
+# would be good additions.
+
+
+# What data structure did you decide to store the accountability groups in and
+# why?
+
+# I chose to store the accountability groups in arrays (and kept those arrays
+# in an array of arrays). I chose this method because I didn't feel there was
+# any need to reference the groups by any other information; the lists
+# themselves are the important things, not the relationships between any kinds
+# of information.
+
+
+# What did you learn in the process of refactoring your initial solution? Did
+# you learn any new Ruby methods?
+
+# I learned that it appears to be much easier to refactor iterative loops than
+# it is to refactor conditional statements. I didn't learn any new methods,
+# but I got some more practice with mapping and shifting. Also, I found a nice
+# resource to help with refactoring at http://ghendry.net/refactor.html
