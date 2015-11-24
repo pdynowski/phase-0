b031d18ec38eb1276dffd2bf6b064b3010ba6a4e
pdynowski <paul.dynowski@gmail.com>
2015-11-24 12:49:01 -0600

    Add refactored solutions

--- week-6/nested_data_structure.rb
+++ week-6/nested_data_structure.rb
@@ -6,7 +6,7 @@ array = [[1,2], ["inner", ["eagle", "par", ["FORE", "hook"]]]]
 
 # attempts:
 # ============================================================
-
+p array[1][1][2][0]
 
 
 # ============================================================
@@ -19,7 +19,7 @@ hash = {outer: {inner: {"almost" => {3 => "congrats!"}}}}
 # attempts:
 # ============================================================
 
-
+p hash[:outer][:inner]["almost"][3]
 
 # ============================================================
 
@@ -32,7 +32,7 @@ nested_data = {array: ["array", {hash: "finished"}]}
 # attempts:
 # ============================================================
 
-
+p nested_data[:array][1][:hash]
 
 # ============================================================
 
@@ -40,8 +40,52 @@ nested_data = {array: ["array", {hash: "finished"}]}
 
 number_array = [5, [10, 15], [20,25,30], 35]
 
-
+number_array.map! do |number|
+  if number.is_a? Array
+    number.map! do |inner_number|
+      inner_number += 5
+    end
+  else
+    number += 5
+  end
+end
+print number_array
 
 # Bonus:
 
 startup_names = ["bit", ["find", "fast", ["optimize", "scope"]]]
+
+# startup_names.map! do |word|
+#   if word.is_a? Array
+#     word.map! do |inner_word|
+#       if inner_word.is_a?(Array)
+#         inner_word.map! do |inner_inner_word|
+#           inner_inner_word += "ly"
+#         end
+#       else
+#         inner_word += "ly"
+#       end
+#     end
+#   else
+#     word += "ly"
+#   end
+# end
+
+
+def add_ly(word)
+  if word.is_a?(Array)
+    word.map! do |word2|
+      add_ly(word2)
+    end
+  else
+    word += "ly"
+  end
+end
+
+startup_names.map! do |word|
+  add_ly(word) 
+end
+
+
+  
+p startup_names
