f2c259813f44edbc806315a0b75a3a223444f7c8
pdynowski <paul.dynowski@gmail.com>
2015-11-25 00:51:58 -0600

    Add refactoring

--- week-6/bingo_solution.rb
+++ week-6/bingo_solution.rb
@@ -39,17 +39,52 @@
 
 # Initial Solution
 
+# class BingoBoard
+
+#   def initialize(board)
+#     @bingo_array = %w(B I N G O)
+#     @bingo_board = board
+#   end
+
+#   def call
+#     @column = Random.new.rand(5)
+#     @number = Random.new.rand(100) + 1
+#     puts "#{@bingo_array[@column]}#{@number}"
+#   end
+
+#   def display_column(col)
+#     @bingo_board.each do |row|
+#       p row[col]
+#     end
+#   end
+
+#   def display_board
+#     @bingo_board.each do |row|
+#       p row
+#     end
+#   end
+
+#   def check(col=@column, num=@number)
+#     @bingo_board.each do |row|
+#       row[col] = 'X' if row[col] == num
+#     end
+#   end
+
+# end
+
+# Refactored Solution
+
 class BingoBoard
 
-  def initialize(board)
-    @bingo_array = %w(B I N G O)
+  def initialize(board=create_board)
     @bingo_board = board
   end
 
   def call
+    bingo_array = %w(B I N G O)
     @column = Random.new.rand(5)
     @number = Random.new.rand(100) + 1
-    puts "#{@bingo_array[@column]}#{@number}"
+    puts "#{bingo_array[@column]}#{@number}"
   end
 
   def display_column(col)
@@ -58,8 +93,8 @@ class BingoBoard
     end
   end
 
-  def display_board
-    @bingo_board.each do |row|
+  def display_board(board=@bingo_board)
+    board.each do |row|
       p row
     end
   end
@@ -70,9 +105,21 @@ class BingoBoard
     end
   end
 
-end
+  def create_board
+    new_board = Array.new(5)
+    b = Array(1..15).shuffle.slice(0,5)
+    i = Array(16..30).shuffle.slice(0,5)
+    n = Array(31..45).shuffle.slice(0,5)
+    n[2] = "FREE"
+    g = Array(46..60).shuffle.slice(0,5)
+    o = Array(61..75).shuffle.slice(0,5)
 
-# Refactored Solution
+    (0..4).each do |x|
+      new_board[x] = [b[x],i[x],n[x],g[x],o[x]]
+    end
+    new_board
+  end
+end
 
 #DRIVER CODE (I.E. METHOD CALLS) GO BELOW THIS LINE
 board = [[47, 44, 71, 8, 88],
@@ -81,12 +128,13 @@ board = [[47, 44, 71, 8, 88],
         [25, 31, 96, 68, 51],
         [75, 70, 54, 80, 83]]
 
-new_game = BingoBoard.new(board)
+new_game = BingoBoard.new()
+# new_game.display_board
+# new_game.call
+# new_game.check
+# new_game.display_board
+a = new_game.create_board
 new_game.display_board
-new_game.call
-new_game.check
-new_game.display_board
-
 
 #Reflection
 
