802733a129526d258ee9abd05840b6b462c014b4
pdynowski <paul.dynowski@gmail.com>
2015-12-03 07:53:28 -0600

    Add finished solution

--- week-7/group_project_solution.js
+++ week-7/group_project_solution.js
@@ -1,25 +1,35 @@
-// User Stories: Calvin Lang
-// As a user would like 3 functions called "sum" , "mean" and "median". I have 2 sets of numbers, one called 
-// "oddLengthArray" and one called "evenLengthArray". I would like each of the named functions to give me their 
-// respective values for each array.
+// Looks like I wasn't supposed to see the user stories, and the next person
+// isn't supposed to see the psuedocode, so it's all gone.
 
-// Pseudocode:  David Ma
-// 1.  Define functions such that they work for both sized arrays
-//    a. The only one that will be weird is median, where we may need some flow control
-//    b. Otherwise, we should be able to create sum / mean in a way that doesn't depend on length of array
-// 2.  "SUM" function on an array
-//    a. Define a function sum that takes an array as an argument 
-//    b. Store the length of the array as a local variable
-//    c. Create a loop that iterates through the array (using the saved array as end of loop)
-//    d. In the loop, define a local variable as the running total (maybe call it sum)
-//    e. With each iteration, increment the index (loop iteration) and add to the running total
-// 3. "MEAN" function on an array
-//    a.  Define a mean function that accepts an array
-//    b.  We can use our SUM function here actually, so save ourselves some code by referencing it
-//    c.  Call the SUM function on the array and save it as a local variable
-//    d.  Divide the output of the SUM function by the length of the array
-// 4. "MEDIAN" function on an array
-//    a.  We first need to be able to sort the array (I just looked and thankfully this method exists...)
-//    b.  Introduce flow control on the length of the array, probably using the modulo and 2 (length % 2)
-//      i. In the event it's odd, modulo will == 1, so we will take return the array's index at (length / 2) (we want integer division)
-//      ii.  In the event that it's even, the modulo == 0, so we will need index (length / 2) and 1 less than that, and we need the mean of that
+var sum = function(numbers) {
+  var length = numbers.length;
+  var sum = 0;
+  for(var index = 0; index < length; index++){
+    sum += numbers[index];
+  }
+  return sum;
+}
+
+var mean = function(numbers) {
+  var total = sum(numbers);
+  return total/numbers.length;
+}
+
+var median = function(numbers) {
+  var sorted = numbers.sort(function(a,b){
+    if (a < b) return -1;
+    if (a > b) return 1;
+    return 0;
+  });
+
+  if(numbers.length % 2 == 0) {
+    return mean(numbers.slice(Math.floor(numbers.length/2)-1,Math.floor(numbers.length/2) + 1));
+  } else {
+    return numbers[Math.floor(numbers.length/2)];
+  }
+}
+
+
+
+
+
