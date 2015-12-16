e18d98389c068f82d3557e8730979ef0257fbd23
pdynowski <paul.dynowski@gmail.com>
2015-12-15 22:30:53 -0600

    Add reflection

--- week-9/review.js
+++ week-9/review.js
@@ -67,13 +67,33 @@ function deleteItem(list, item) {
 
 // TEST CODE
 
-console.log(newList("steak arugula salmon onions lemons"));
-var groceries = newList("steak arugula salmon onions lemons");
-addItem(groceries, "cheese");
-console.log(groceries);
-addItem(groceries, "pickles", -2);
-console.log(groceries);
-updateItem(groceries, "arugula", -2);
-console.log(groceries);
-deleteItem(groceries, "cheese");
-console.log(groceries);
+// console.log(newList("steak arugula salmon onions lemons"));
+// var groceries = newList("steak arugula salmon onions lemons");
+// addItem(groceries, "cheese");
+// console.log(groceries);
+// addItem(groceries, "pickles", -2);
+// console.log(groceries);
+// updateItem(groceries, "arugula", -2);
+// console.log(groceries);
+// deleteItem(groceries, "cheese");
+// console.log(groceries);
+
+/* REFLECTION
+
+What concepts did you solidify in working on this challenge? (reviewing the
+passing of information, objects, constructors, etc.)
+
+General javascript method work, etc - passing variables and the like. I also
+learned that the class constructor for JS doesn't seem to work in node for
+some reason - although it's supposedly supported soon.
+
+What was the most difficult part of this challenge?
+
+I had originally done some work using an assert function, rather than test
+code, but I couldn't really get it to work like I wanted. Other than that, the
+JS itself was pretty straightforward.
+
+Did an array or object make more sense to use and why?
+
+Object - it made a lot more sense to use the list items as keys and the
+quantities as the values than to create subarrays or something.
